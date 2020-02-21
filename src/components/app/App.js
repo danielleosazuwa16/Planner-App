f77a8897ef819c111e1e903fad456df4fbbcc2b3
danielleosazuwa16 <danielleosazuwa16@augustana.edu>
2020-02-20 22:23:54 -0600

    Added a few material components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MenuBar from '../MenuBar/MenuBar';
 import Divider from '@material-ui/core/Divider';
+import { Card } from '@material-ui/core';
 
 function App() {
   return (
     <React.Fragment>
       <MenuBar></MenuBar>
-      <Divider />
+      <Divider variant="middle"/>
     </React.Fragment>
   );
 }
