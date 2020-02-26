bb44ed120e596fe5ed93403199dc4223b9352027
Danielle Osazuwa <danielleosazuwa16@augustana.edu>
2020-02-26 12:38:21 -0600

    DatePicker component now works

--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -1,7 +1,7 @@
-import 'date-fns';
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
+
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
