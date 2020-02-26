dafc18d030b8fc7b7ada2b609efd544ebbee05da
Danielle Osazuwa <danielleosazuwa16@augustana.edu>
2020-02-26 11:35:44 -0600

    Updated package.json and created some data classes

---
+++ src/data/TableData.js
@@ -0,0 +1,7 @@
+//TODO: not quite sure what's to be included
+
+export interface PlanningTableData {
+    headers: String[],
+    dateRange: Date[],
+    data: String[]
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './components/App/App';
+import App from './components/app/App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
